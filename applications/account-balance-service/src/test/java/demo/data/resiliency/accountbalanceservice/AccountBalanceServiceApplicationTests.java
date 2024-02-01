8325973694d104f7405dfce3fcb961636e8a981b
ggreen <gregoryg@vmware.com>
2024-02-01 08:37:57 -0500

    initial commit

---
+++ applications/account-balance-service/src/test/java/demo/data/resiliency/accountbalanceservice/AccountBalanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.data.resiliency.accountbalanceservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountBalanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
